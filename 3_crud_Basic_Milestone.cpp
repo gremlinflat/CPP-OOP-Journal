4dd6a8010c987a76f70d2b0da867d5d505b1659a
gremlinflat <fahri.119140205@student.itera.ac.id>
2020-06-22 01:40:54 +0700

    FUCK I DONT KNOW WHAT IM DOING

--- 3_crud_Basic_Milestone.cpp
+++ 3_crud_Basic_Milestone.cpp
@@ -5,33 +5,50 @@
 
 using namespace std;
 
+struct student{
+    int pk; //primary key
+    string NIM;
+    string name;
+    string major;
+};
+
+void writeDatabase(fstream &database, int position, student &inputStudent){
+    database.seekp( (position-1) sizeof(inputStudent) , ios::beg);
+    database.write(reinterpret_cast<char*>(&inputStudent), sizeof(inputStudent));
+}
+
+void addDatabase(fstream &database){
+    student inputStudent; // creating data type from struct
+    inputStudent.pk = 1;
+    cout<< "Name : ";
+    getline(cin, inputStudent.name);
+    cout<< "NIM : ";
+    getline(cin, inputStudent.NIM);
+    cout<< "Major : ";
+    getline(cin, inputStudent.major);
+
+    writeDatabase(database, position, inputStudent);
+}
 int getMenu();
+void checkDatabase(fstream &data);
 
 int main(){
     
     fstream database;
 
-    database.open("data.bin", ios::out | ios::in | ios::binary);
-
-    if(database.is_open()){
-        cout << "Database found"<<endl;
-    }else{
-        cout << "Database not found, Creating new database"<<endl;
-        database.close();
-        database.open("data.bin", ios::trunc | ios::out | ios::in | ios::binary);
-
-    }
+    checkDatabase(database); //checking database and opening data base
 
     int pilihan = getMenu();
     char is_continue;
 
-    enum option{CREATE = 1, READ, UPDATE, DELETE, FINISH};
+    enum option{CREATE = 1, READ, UPDATE, DELETE, FINISH};//enum array
     
-    while (pilihan != FINISH){
+    while (pilihan != FINISH){//if its not finished end
 
         switch (pilihan){
         case CREATE:
-            
+            cout << "CREATE A NEW DATA"<<endl;
+            addDatabase(database);
             break;
         case READ:
             
@@ -48,37 +65,47 @@ int main(){
         }
 
         cout<<" continue? (y/n) : ";
-        std::cin>> is_continue;
+        std::cin>> is_continue; //asking for confirmation
+
         if(is_continue=='y'){
-            pilihan = getMenu();
+            pilihan = getMenu(); //reshow menu and asking for input
         }
         if(is_continue=='n'){
-            break;
-        }else{
-            cout<<"INVALID OPTION!!";
+            break; //exit the loop ending program
         }
 
     }
-    
 
-    std::cin.get();
     return 0;
 }
 
 
 int getMenu(){
     int input;
-    system("cls");
+    system("cls"); //clearing screen
     cout << "\nMilestone project CRUD" <<endl;
     cout << "========================"<<endl;
-    cout << "1. APPEND NEW DATA" <<endl;
-    cout << "2. SHOW ENTIRE DATA TABLE" <<endl;
-    cout << "3. CHANGE DATA" <<endl;
+    cout << "1. CREATE NEW DATA" <<endl;
+    cout << "2. READ ENTIRE DATA TABLE" <<endl;
+    cout << "3. UPDATE DATA" <<endl;
     cout << "4. DELETE DATA" <<endl;
     cout << "5. CLOSE " <<endl;
     cout << "========================"<<endl;
     cout << "Option [1-5] : ";
     
     std::cin>>input;
+    cin.ignore(1000, '\n');//flushing the input after enter
     return input;
+}
+
+void checkDatabase(fstream &database){
+    database.open("data.bin", ios::out | ios::in | ios::binary); //opening database if it exist
+
+    if(database.is_open()){
+        cout << "Database found"<<endl;
+    }else{
+        cout << "Database not found, Creating new database"<<endl;
+        database.close(); // closing database and create new
+        database.open("data.bin", ios::trunc | ios::out | ios::in | ios::binary);//opening database if it doesnt exist and creating new file
+    }
 }
